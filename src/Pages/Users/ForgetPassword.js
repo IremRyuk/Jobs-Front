55fb2f8e7a326bdb3903bd683625d91e20330977
IremRyuk <IremRyuk>
2023-08-31 22:13:14 +0400

    change design

--- src/Pages/Users/ForgetPassword.js
+++ src/Pages/Users/ForgetPassword.js
@@ -1,8 +1,10 @@
-import { TextField,Stack,Button } from '@mui/material'
-import React, { useState } from 'react'
+import { TextField,Stack,Button,Box,Typography } from '@mui/material'
+import React, { useState , useEffect } from 'react'
 
 export default function ForgetPassword() {
     const [gmail,setGmail] = useState('')
+    const [status,setStatus] = useState(null)
+    const [color,setColor] = useState('red')
     // Reset Password
     const resetPassword = async () => {
         const response = await fetch('https://jobs-2dwq.onrender.com/forgetpassword/',{
@@ -13,26 +15,84 @@ export default function ForgetPassword() {
           }
         })
         const json = await response.json()
-        if(!response){
-          console.log('Error')
-        }else{
-          console.log(json.Status)
-        }
+        setStatus(json)
     }
-    console.log(gmail)
+
+// Mui Styles
+    const forgetTyp = {
+      fontSize:{xs:'25px',sm:'25px',md:'32px',xl:'35px'},
+      fontWeight:{xs:200,md:500,xl:700}
+    }
+    const forgetTyp2 = {
+      fontSize:{xs:'20px',sm:'20px',md:'27px',xl:'30px'},
+      fontWeight:{xs:200,md:500,xl:400}
+    }
+    useEffect(()=>{
+      if(!status){
+        return
+      }else{
+        if(status.St2===true){
+          setColor('green')
+        }else{
+          setColor('red')
+        }
+      }
+
+    },[status])
+    const errorTyp = {
+      fontSize:{xs:'25px',md:'35px'},
+      fontWeight:{xs:400,md:500,xl:400},
+      color:color
+    }
   return (
-    <Stack direction='column' justifyContent='center' alignItems='center'>
+    <Box 
+    display='flex'
+    justifyContent='center'
+    alignItems='center'
+    sx={{
+      width:'100vw',
+      height:'80vh'
+    }}
+    >
+      <Stack
+    direction='column' 
+    justifyContent='space-evenly'
+    alignItems='center'
+    boxShadow='0px 0px 20px silver'
+    borderRadius='10mm'
+    sx={{
+      width:{xs:'90vw',sm:'80vw',lg:'fit-content'},
+      height:{xs:'70vh',sm:'50vh',md:'50vh'},
+      padding:{xs:'20px',sm:'40px 50px',md:'30px 100px'}
+    }}
+      >
+        <Typography sx={forgetTyp}>Forget Password</Typography>
+        <Typography sx={forgetTyp2}>Please Fill Input With Your Gmail</Typography>
         <TextField 
-        placeholder='email'
+        placeholder='type...'
         variant='outlined'
+        label='Gmail'
+        autoComplete='off'
         value={gmail}
         onChange={(e)=>setGmail(e.target.value)}
         sx={{
-            width:'70%'
+            width:{xs:'100%',md:'40vw',xl:'25vw'}
         }}
         />
-        <Button onClick={resetPassword}>Submit</Button>
-    </Stack>
+        <Button 
+        onClick={resetPassword}
+        disableTouchRipple
+        variant='contained'
+        sx={{
+          fontSize:{xs:'15px',md:'17px',xl:'20px'},
+width:{xs:'40vw',md:'30vw',xl:'20vw'}
+        }}
+        >Submit</Button>
+{status && <>
+<Typography sx={errorTyp}>{status.Status}</Typography>
+</>}
+        </Stack>
+    </Box>
 
   )
 }
--- src/Pages/Users/ResetPassword.js
+++ src/Pages/Users/ResetPassword.js
@@ -1,11 +1,20 @@
-import {Stack,TextField,Button} from '@mui/material'
-import React, {useState} from 'react'
-import {useParams} from 'react-router-dom'
+import { TextField,Stack,Button,Box,Typography,InputAdornment} from '@mui/material'
+import React, {useState,useEffect} from 'react'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {useNavigate, useParams} from 'react-router-dom'
 
 
 export default function ResetPassword() {
   const {id,token} = useParams()
   const [password,setPass] =  useState('')
+  const [status,setStatus] = useState(null)
+  const [color,setColor] = useState('red')
+  const [types,setTypes] = useState(false)
+  const [changed,setChanged] = useState(false)
+// Navigation
+const navigate = useNavigate()
+
   const changePassword = async () => {
     const response = await fetch(`https://jobs-2dwq.onrender.com/forgetpassword/${id}/${token}`,{
       method:"POST",
@@ -14,21 +23,134 @@ export default function ResetPassword() {
         'Content-Type':'application/json'
       }
     })
-    if(!response){
-      alert('Problem')
-    }else{
-      alert('Success')
-    }
+    const json = await response.json()
+    setStatus(json)
   }
+
+// Mui Styles
+  const forgetTyp = {
+    fontSize:{xs:'25px',md:'32px',xl:'35px'},
+    fontWeight:{xs:200,md:500,xl:700},
+    textAlign:'center'
+  }
+  const errorTyp = {
+    fontSize:{xs:'25px',md:'35px'},
+    fontWeight:{xs:400,md:500,xl:400},
+    color:color
+  }
+  useEffect(()=>{
+    if(!status){
+      return
+    }else{
+      if(status.St2===true){
+        setColor('green')
+        setTimeout(() => {
+          setChanged(true)
+        }, 1000);
+      }else{
+        setColor('red')
+      }
+    }
+  },[status])
   return (
-    <Stack direction='column' justifyContent='center' alignItems='center'>
-      <TextField 
-      variant='outlined'
-      placeholder='type new password'
-      value={password}
-      onChange={(e)=>setPass(e.target.value)}
-      />
-        <Button onClick={changePassword}>Submit</Button>
+    <>
+    {changed
+    ?
+    <>
+    <Stack
+    direction='column' 
+    justifyContent='center'
+    alignItems='center'
+    sx={{
+      display:'flex',
+      width:'100vw',
+      height:'80vh'
+    }}
+    >
+            <Stack
+    direction='column' 
+    justifyContent='center'
+    alignItems='center'
+    gap='20px'
+    boxShadow='0px 0px 10px gray'
+    borderRadius='10mm'
+    sx={{
+      width:{xs:'90vw',sm:'80vw',lg:'fit-content'},
+      height:{xs:'70vh',sm:'50vh',md:'40vh'},
+      padding:{xs:'10px',sm:'20px 25px',md:'15px 100px'}
+    }}
+      >
+    <Typography sx={forgetTyp}>Click Here To Go Home Page</Typography>
+    <Button 
+    variant='contained'
+    color='primary'
+    sx={{
+    fontSize:{xs:'25px',md:'35px'},
+    fontWeight:{xs:400,md:500,xl:400},
+    }} 
+    onClick={()=>navigate('/')}>Home Page</Button>
     </Stack>
+    </Stack>
+    </>
+    :<Box
+    justifyContent='center'
+    alignItems='center'
+    sx={{
+      display:'flex',
+      width:'100vw',
+      height:'80vh'
+    }}
+    >
+      <Stack
+    direction='column' 
+    justifyContent='space-evenly'
+    alignItems='center'
+    boxShadow='0px 0px 20px silver'
+    borderRadius='10mm'
+    sx={{
+      width:{xs:'90vw',sm:'80vw',lg:'fit-content'},
+      height:{xs:'70vh',sm:'50vh',md:'50vh'},
+      padding:{xs:'20px',sm:'40px 50px',md:'30px 100px'}
+    }}
+      >
+        <Typography sx={forgetTyp}>Reset Password</Typography>
+        <TextField 
+        type={types?'text':'password'}
+        placeholder='type...'
+        variant='outlined'
+        label='Password'
+        autoComplete='off'
+        value={password}
+        onChange={(e)=>setPass(e.target.value)}
+        sx={{
+            width:{xs:'100%',md:'40vw',xl:'25vw'}
+        }}
+        InputProps={{
+          style:{
+fontSize:'large'
+          },
+          endAdornment:(
+            <InputAdornment position='end' onClick={()=>setTypes(e=>!e)} style={{cursor:'pointer'}}>
+              {types?<VisibilityOffIcon/>:<VisibilityIcon />}
+            </InputAdornment>
+          )
+        }}
+        />
+        <Button 
+        onClick={changePassword}
+        disableTouchRipple
+        variant='contained'
+        sx={{
+          fontSize:{xs:'15px',md:'17px',xl:'20px'},
+width:{xs:'40vw',md:'30vw',xl:'20vw'}
+        }}
+        >Submit</Button>
+{status && <>
+<Typography sx={errorTyp}>{status.Status}</Typography>
+</>}
+        </Stack>
+    </Box>
+  }
+  </>
   )
 }
